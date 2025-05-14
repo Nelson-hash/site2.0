b9f3e48ecd21c8acf871057e32472a61e1e98802
Nelson-hash <nlsn.remy@gmail.com>
2025-05-14 14:58:44 +0200

    Update NavigationItem.tsx

--- src/components/NavigationItem.tsx
+++ src/components/NavigationItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { useCursor } from '../context/CursorContext';
 
@@ -12,17 +13,20 @@ const NavigationItem: React.FC<NavigationItemProps> = ({ title, href, position }
   const { setHovered } = useCursor();
   
   return (
-    <motion.a
-      href={href}
-      className="nav-item"
+    <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 0.8 }}
       transition={{ duration: 1, delay: 0.5 }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
     >
-      {title}
-    </motion.a>
+      <Link 
+        to={href}
+        className="nav-item"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        {title}
+      </Link>
+    </motion.div>
   );
 };
 
