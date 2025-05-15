2a1bb2ccbaef2ae24aec18b5b462f8464285f966
Nelson-hash <nlsn.remy@gmail.com>
2025-05-15 12:23:19 +0200

    Update NavigationItem.tsx

--- src/components/NavigationItem.tsx
+++ src/components/NavigationItem.tsx
@@ -10,19 +10,30 @@ interface NavigationItemProps {
 }
 
 const NavigationItem: React.FC<NavigationItemProps> = ({ title, href, position }) => {
-  const { setHovered } = useCursor();
+  const { setHovered, isMobile } = useCursor();
+  
+  // Mobile touch event handlers
+  const handleTouch = () => {
+    // On mobile, we can briefly flash the "hovered" state on touch
+    if (isMobile) {
+      setHovered(true);
+      setTimeout(() => setHovered(false), 300);
+    }
+  };
   
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 0.8 }}
       transition={{ duration: 1, delay: 0.5 }}
+      className={isMobile ? "py-3" : ""}
     >
       <Link 
         to={href}
-        className="nav-item"
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
+        className={`nav-item ${isMobile ? 'text-xl py-2 px-4' : ''}`}
+        onMouseEnter={() => !isMobile && setHovered(true)}
+        onMouseLeave={() => !isMobile && setHovered(false)}
+        onTouchStart={handleTouch}
       >
         {title}
       </Link>
