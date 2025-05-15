fd4d66f452a2bea0afa9a2b2b14455f00e54e428
Nelson-hash <nlsn.remy@gmail.com>
2025-05-15 12:29:39 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Navigation from './Navigation';
 import Logo from './Logo';
+import { useCursor } from '../context/CursorContext';
 
 const Layout: React.FC = () => {
+  const { isMobile } = useCursor();
+
   return (
     <div className="relative h-full w-full">
       <Navigation />
-      <div className="h-full flex items-center justify-center">
-        <Logo />
+      <div className={`h-full flex items-center justify-center ${isMobile ? 'mt-0' : ''}`}>
+        {/* Add explicit centering for mobile */}
+        <div className={`${isMobile ? 'absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' : ''}`}>
+          <Logo />
+        </div>
       </div>
     </div>
   );
