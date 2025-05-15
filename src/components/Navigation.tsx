9a276f2d7c69e06a3af0bd3988cec09b83184b2d
Nelson-hash <nlsn.remy@gmail.com>
2025-05-15 12:29:14 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,10 +5,10 @@ import { useCursor } from '../context/CursorContext';
 const Navigation: React.FC = () => {
   const { isMobile } = useCursor();
 
-  // Mobile navigation layout with improved touch targets
+  // Mobile navigation layout with improved visibility and larger text
   if (isMobile) {
     return (
-      <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-between bg-black bg-opacity-70 z-20">
+      <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-between items-center bg-black bg-opacity-80 z-20">
         <NavigationItem title="FILMS" href="/films" position="bottom-left" />
         <NavigationItem title="A PROPOS" href="/about" position="bottom-right" />
       </div>
