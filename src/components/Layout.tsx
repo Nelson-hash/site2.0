55d7a4b5cff8c14c50759ecd32a7bbdc5cd0b610
Nelson-hash <nlsn.remy@gmail.com>
2025-07-24 11:09:41 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './Navigation';
 import Logo from './Logo';
 import { useCursor } from '../context/CursorContext';
 
 const Layout: React.FC = () => {
   const { isMobile } = useCursor();
+
+  // Reset scroll position when component mounts (returning to landing page)
+  useEffect(() => {
+    if (isMobile) {
+      window.scrollTo(0, 0);
+    }
+  }, [isMobile]);
 
   return (
     <div className="relative h-full w-full">
