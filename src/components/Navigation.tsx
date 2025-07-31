5f9c4f18d5046a7541b51718052f273faba3fecf
Nelson-hash <nlsn.remy@gmail.com>
2025-07-31 16:54:46 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavigationItem from './NavigationItem';
+import Logo3D from './Logo3D';
 import { useCursor } from '../context/CursorContext';
 
 const Navigation: React.FC = () => {
@@ -8,17 +9,31 @@ const Navigation: React.FC = () => {
   // Mobile navigation layout with improved visibility and larger text
   if (isMobile) {
     return (
-      <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-between items-center bg-black bg-opacity-80 z-20 mobile-nav-bar">
-        <NavigationItem title="FILMS" href="/films" position="bottom-left" />
-        <NavigationItem title="A PROPOS" href="/about" position="bottom-right" />
-      </div>
+      <>
+        {/* 3D Logo in top right corner */}
+        <div className="fixed top-4 right-4 z-30">
+          <Logo3D />
+        </div>
+        
+        {/* Bottom navigation bar */}
+        <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-between items-center bg-black bg-opacity-80 z-20 mobile-nav-bar">
+          <NavigationItem title="FILMS" href="/films" position="bottom-left" />
+          <NavigationItem title="A PROPOS" href="/about" position="bottom-right" />
+        </div>
+      </>
     );
   }
 
-  // Desktop navigation (unchanged)
+  // Desktop navigation
   return (
     <div className="absolute inset-0 p-6 md:p-10">
       <div className="relative h-full w-full">
+        {/* Top right 3D logo */}
+        <div className="absolute top-0 right-0">
+          <Logo3D />
+        </div>
+        
+        {/* Navigation items */}
         <div className="absolute top-0 left-0">
           <NavigationItem title="FILMS" href="/films" position="top-left" />
         </div>
