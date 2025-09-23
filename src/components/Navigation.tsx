f8fffb2b7be1302afc3b48239607c01129bcd351
Nelson-hash <nlsn.remy@gmail.com>
2025-09-23 13:49:03 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import NavigationItem from './NavigationItem';
-import Logo3D from './Logo3D';
 import { useCursor } from '../context/CursorContext';
 
 const Navigation: React.FC = () => {
   const { isMobile } = useCursor();
 
-  // Mobile navigation layout with improved visibility and larger text
+  // Mobile navigation layout - removed 3D logo
   if (isMobile) {
     return (
       <>
-        {/* 3D Logo in top right corner */}
-        <div className="fixed top-4 right-4 z-30">
-          <Logo3D />
-        </div>
-        
         {/* Bottom navigation bar */}
         <div className="fixed bottom-0 left-0 right-0 p-4 flex justify-between items-center bg-black bg-opacity-80 z-20 mobile-nav-bar">
           <NavigationItem title="FILMS" href="/films" position="bottom-left" />
@@ -24,15 +18,10 @@ const Navigation: React.FC = () => {
     );
   }
 
-  // Desktop navigation
+  // Desktop navigation - removed 3D logo
   return (
     <div className="absolute inset-0 p-6 md:p-10">
       <div className="relative h-full w-full">
-        {/* Top right 3D logo */}
-        <div className="absolute top-0 right-0">
-          <Logo3D />
-        </div>
-        
         {/* Navigation items */}
         <div className="absolute top-0 left-0">
           <NavigationItem title="FILMS" href="/films" position="top-left" />
