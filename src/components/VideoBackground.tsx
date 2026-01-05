a2e315b86a8f8c6573a3b5185f9372665be55022
Nelson-hash <nlsn.remy@gmail.com>
2026-01-05 14:37:59 +0100

    Update VideoBackground.tsx

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -4,7 +4,7 @@ const VideoBackground: React.FC = () => {
   return (
     <div className="fixed inset-0 z-[-1]">
       <img
-        src="/videos/background-video2.mp4"
+        src="/images/background.gif"
         alt="Background animation"
         className="video-background w-full h-full object-cover"
         style={{ filter: 'brightness(0.4) contrast(1.1)' }}
